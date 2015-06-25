34b4afa7b1195c75df5388051c7071af246b13dd
Fellipe Castro <contact@fellipecastro.com>
2015-06-25 09:18:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#git clone https://github.com/fellipecastro/vim-ide.git ~/.vim-ide
-#mkdir -p ~/.vim-ide/vim/bundle
+git clone https://github.com/fellipecastro/vim-ide.git ~/.vim-ide -f
+mkdir -p ~/.vim-ide/vim/bundle -f
 ln -snf ~/.vim-ide/vimrc ~/.vimrc
 ln -snf ~/.vim-ide/vim ~/.vim
 
-git clone --depth 1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth 1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim -f
 vim +PluginInstall +qall
