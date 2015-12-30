384ef7acb3c9e3140d78f4dfadca0ed1065bb5b7
Fellipe Castro <contact@fellipecastro.com>
2015-12-30 15:33:35 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-get install exuberant-ctags vim vim-nox -y
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  brew install ctags-exuberant vim --with-lua --override-system-vi
+  brew install ctags-exuberant vim --with-lua # --override-system-vi
 fi
 git clone https://github.com/fellipecastro/vim-ide.git ~/.vim-ide
 mkdir -p ~/.vim-ide/.vim/bundle
