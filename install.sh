01821e0441c4f641f1a9b4f3abe18b0eff229487
Fellipe Castro <contact@fellipecastro.com>
2016-01-02 11:03:52 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+rm -rf ~/vim-ide
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install exuberant-ctags vim vim-nox -y
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install ctags-exuberant vim --with-lua --override-system-vi
 fi
+
 git clone https://github.com/fellipecastro/vim-ide.git ~/vim-ide
+
 mkdir -p ~/vim-ide/vim/bundle
+
 ln -snf ~/vim-ide/.vimrc ~/.vimrc
 ln -snf ~/vim-ide/vim ~/.vim
+
 touch ~/.vimrc_extra
+
 git clone --depth 1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 vim +PluginInstall +qa
+
 if [[ "$SHELL" == "/bin/zsh" ]]; then
     rehash
 elif [[ "$SHELL" == "/bin/bash" ]]; then
