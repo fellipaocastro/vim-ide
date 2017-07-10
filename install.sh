d25141b0b90724e32ee5e3fd2605f206916bd681
Fellipe Castro <contact@fellipecastro.com>
2017-07-10 16:48:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install exuberant-ctags vim vim-nox -y
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install ctags-exuberant
+    brew install perl
+    brew install python
+    brew install ruby
     brew install vim --with-override-system-vi
 fi
 
